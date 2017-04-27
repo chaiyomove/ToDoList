b7403e5a1bf229505b6a39fa9f0f546cf7b97ad3
Sumet Benjawan <chaiyo_move83@hotmail.com>
2017-04-27 23:24:48 +0700

    Delete unused documentation

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -171,14 +171,6 @@ class TasksController extends Controller
     {
         //
     }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     
     /**
      * Update status field in storage.
